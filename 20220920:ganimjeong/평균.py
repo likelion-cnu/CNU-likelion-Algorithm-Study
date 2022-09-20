1edfcf6037ecd19ebc794692f305a4a5db2a6b5b
ganimjeong <86656605+ganimjeong@users.noreply.github.com>
2022-09-20 17:43:01 +0900

    [Baekjoon-1546] 평균

---
+++ 20220920:ganimjeong/평균.py
@@ -0,0 +1,9 @@
+n = int(input()) 
+orilist = list(map(int, input().split()))
+M = max(orilist)
+newlist =[]
+for i in orilist:
+    newlist.append(i/M*100)
+    
+average = sum(newlist)/n
+print(average)
