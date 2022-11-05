f22db7b3de62f24d5b5dbfd5c03d9fd7678aaae2
moonmonoon <moonmonoon@gmail.com>
2022-11-06 00:43:31 +0900

    4주차 커밋

---
+++ Programmers/moonmonoon/week_1/완주하지못한선수.py
@@ -0,0 +1,22 @@
+def solution(participant, completion):
+    hash1 = {}
+    hash2 = {}
+    answer = ""
+
+    for key in participant:
+        if key in hash1:
+            hash1[key] += 1
+        else:
+            hash1[key] = 1
+    # print(hash1)
+
+    for key in completion:
+        if key in hash1:
+            hash1[key] -= 1
+    # print(hash1)
+
+    for key in hash1:
+        if hash1[key] != 0:
+            answer = key
+
+    return answer
